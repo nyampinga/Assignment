ce5364dc4ba359ce5fc782a1799f30e71ac4fad7
nyampinga <joselynenya@gmail.com>
2021-08-26 10:33:21 +0200

    react

---
+++ REACTAPP/freementor-ui/src/views/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../component/Header";
+import OurProfile from "../component/OurProfile";
+const Home =()=>{
+
+    return(
+        <div>
+            <Header/>
+            <OurProfile/>
+        </div>
+    )
+}
+
+export default Home;
